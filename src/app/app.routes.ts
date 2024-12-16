c366a3eb03e068a8ce3d3be75e617c60acab2487
yurights <yuriibpk@gmail.com>
2024-12-16 17:25:26 +0200

    chore: login component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './layouts/login/login.component';
+import { ChatComponent } from './layouts/chat/chat.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent },
+    {path: '', component: ChatComponent },
+];
