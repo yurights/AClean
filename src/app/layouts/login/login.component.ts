716f9f443f5606beaf8b368b30c329e659d3d0b3
yurights <yuriibpk@gmail.com>
2024-12-16 19:47:59 +0200

    chore: login form fields added

--- src/app/layouts/login/login.component.ts
+++ src/app/layouts/login/login.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,11 +1,9 @@
 import { Injectable } from '@angular/core';
 import {
-  ActivatedRouteSnapshot,
   CanActivate,
   GuardResult,
   MaybeAsync,
   Router,
-  RouterStateSnapshot,
 } from '@angular/router';
 import { AuthService } from './auth.service';
 
@@ -15,16 +13,14 @@ import { AuthService } from './auth.service';
 export class AuthGuard implements CanActivate {
   constructor(private router: Router, private auth: AuthService) {}
   canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
+    // route: ActivatedRouteSnapshot,
+    // state: RouterStateSnapshot
   ): MaybeAsync<GuardResult> {
-    if (this.auth.isLogined()){
+    if (!this.auth.isLogined()) {
+      this.router.navigate(['login']);
+      return false;
+    } else {
       return true;
     }
-    console.log(route);
-    console.log(state);
-    const path = state.url;
-    console.log('___---> ', path);
-    return false;
   }
 }
